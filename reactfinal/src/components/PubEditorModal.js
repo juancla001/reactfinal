18c0dfb9fb78b87fd14c1685029d6206c2d73ee7
juanClavijo <juancheloar@gmail.com>
2021-08-01 21:52:07 -0300

    subir imagen, cargar form.

--- reactfinal/src/components/PubEditorModal.js
+++ reactfinal/src/components/PubEditorModal.js
@@ -6,7 +6,18 @@ import Form from 'react-bootstrap/Form';
 
 
 export default function PubEditorModal(props) {
-    const [categorias, setCategorias] = useState([]) ;
+    const [categorias, setCategorias] = useState([]);
+
+    const [pubTitulo, setPubTitulo] = useState('');
+    const [pubPrice, setPubPrice] = useState('');
+
+    const [pubImage, setPubImage] = useState('');
+    const [previewImage, setPreviewImage] = useState('');
+
+    const [pubCategory, setPubCategory] = useState('');
+
+    const [pubDescription, setPubDescription] = useState('');
+
     useEffect(() => {
         const url = 'http://localhost:8000/categorias';
 
@@ -27,7 +38,48 @@ export default function PubEditorModal(props) {
         .then((response)=>response.json())
         .then((categorias)=>{
         });
-    }
+    };
+
+    const handletPutTituloChange = (event)=>{
+        setPubTitulo(event.target.value)
+    };
+
+    const handletPutPriceChange = (event)=>{
+        setPubPrice(event.target.value)
+    };
+
+    const handleCategoryChange = (event)=>{
+        setPubCategory(event.target.value)
+    };
+
+    const handletPutDescriptionChange = (event)=>{
+        setPubDescription(event.target.value)
+    };
+
+    const handlePubImageChange = (event)=>{
+        setPubImage(event.target.files[0]);
+
+    setPreviewImage(URL.createObjectURL(event.target.files[0]));
+    };
+
+const handleSave = () =>{
+    const formData = new FormData();
+    formData.append('pubTitulo', pubTitulo);
+    formData.append('pubPrice', pubPrice);
+    formData.append('pubImage', pubImage);
+    formData.append('pubCategory', pubCategory);
+    formData.append('pubDescription', pubDescription);
+
+    const url = 'https://localhost:8000/publicaciones'
+    fetch(url,{
+        method: 'POST',
+        body: formData,
+        credentials: 'include'
+    }).then((response) => response.json())
+    .then((data) =>{
+        console.log(data);
+    });
+};
 
     return (
         <Modal show={props.show} onHide={props.handleHide}>
@@ -39,40 +91,53 @@ export default function PubEditorModal(props) {
                 <Form>
                     <Form.Group>
                         <Form.Label>Titulo</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text" 
+                                    value={pubTitulo} 
+                                    onChange={handletPutTituloChange} ></Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Precio</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text"
+                                    value={pubPrice} 
+                                    onChange={handletPutPriceChange}></Form.Control>
                     </Form.Group>
                     <Form.Group className="d-flex justify-content-center">
-                        <img style={{ height:'30vh'}}
-                            src="http://localhost:8000/images/branca1.jpg" />
+                        {previewImage &&(
+                            <img style={{ height:'30vh'}}
+                            src={previewImage} />
+                            )}
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Categoria</Form.Label>
-                        <Form.Control as="select">
+                        <Form.Control as="select" value={pubCategory} onChange={handleCategoryChange}>
                             {getCategoriasOptions()}
                         </Form.Control>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Image</Form.Label>
-                        <Form.Control type="file" ></Form.Control>
+                        <Form.Control type="file" 
+                                    onChange={handlePubImageChange} ></Form.Control>
                     </Form.Group>
                 </Form>
+                <Form.Group>
+                        <Form.Label>Descripcion</Form.Label>
+                        <Form.Control type="text"
+                                    value={pubDescription} 
+                                    onChange={handletPutDescriptionChange}></Form.Control>
+                    </Form.Group>
 
             </Modal.Body>
 
             <Modal.Footer>
-                <Button variant="secundary" onHide={props.handleHide}>
+                <Button variant="secondary" onHide={props.handleHide}>
                     Cancelar
                 </Button>
-                <Button>Guardar</Button>
+                <Button variant="primary" onClick={handleSave}>Guardar</Button>
             </Modal.Footer>
 
         </Modal>
-    )
-}
+    );
+};
 
